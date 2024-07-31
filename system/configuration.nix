ba2e3751c80c936dfafb90f831af728f7b183320
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2024-07-31 13:29:44 -0300

    enabling appimages

--- system/configuration.nix
+++ system/configuration.nix
@@ -14,6 +14,15 @@
 #  boot.loader.systemd-boot.enable = true;
 #  boot.loader.efi.canTouchEfiVariables = true;
 
+	boot.binfmt.registrations.appimage = {
+		wrapInterpreterInShell = false;
+		interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+		recognitionType = "magic";
+		offset = 0;
+		mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+		magicOrExtension = ''\x7fELF....AI\x02'';
+	};
+
   powerManagement.cpuFreqGovernor = "performance";
   powerManagement.cpufreq.min = 41000;
 
