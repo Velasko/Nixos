468d94ce054ad114a2b011db61685d41435dcefb
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2024-08-09 19:10:19 -0300

    wallpaper path fixed

--- home/velasco/modules/stylix.nix
+++ home/velasco/modules/stylix.nix
@@ -1,9 +1,9 @@
-{ config, pkgs, ... } : {
+{ username, config, pkgs, ... } : {
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/pop.yaml";
   stylix.cursor.package = pkgs.apple-cursor;
   stylix.cursor.name = "macOS-BigSur-White";
 
-  stylix.image = ../../home/${username}/wallpaper.png;
+  stylix.image = ../../../home/${username}/wallpaper.png;
   stylix.polarity = "dark";
   stylix.enable = true;
 }
