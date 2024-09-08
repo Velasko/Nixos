7b9354dac7a01be23b42a62520688ccd7bb300ba
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2024-09-08 00:11:57 -0300

    Fixing disko install config url

--- disko-install.sh
+++ disko-install.sh
@@ -6,7 +6,7 @@ DISKPATH="/tmp/disko.nix"
 curl https://raw.githubusercontent.com/Velasko/Nixos/main/nix-clear.sh | bash -
 
 
-curl https://raw.githubusercontent.com/Velasko/Nixos/main/system/disko/disko-config.nix -o $DISKPATH
+curl https://github.com/Velasko/Nixos/blob/bb13a84f8c8d57d5a9ac88c8ed8f37f6ed26d854/system/disko/virtualized.nix -o $DISKPATH
 sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko $DISKPATH
 
 sudo nixos-install \
