adcdb3cca6f721ca7b501b36bc2869aedee7c881
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2024-09-12 18:27:30 -0300

    idk, just trying go adjust to the doc
    https://nix-community.github.io/NixOS-WSL/how-to/nix-flakes.html

--- system/bootless/configuration.nix
+++ system/bootless/configuration.nix
@@ -1,8 +1,9 @@
-{ wsl, ... }:
+{ pkgs, ... }:
 let
-  sys_config = if wsl then [ ./wsl-configuration.nix ] else [ ];
+  sys_config = if true then [ ./wsl-configuration.nix ] else [ ];
 in
 {
   imports = sys_config;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
 
--- system/bootless/wsl-configuration.nix
+++ system/bootless/wsl-configuration.nix
@@ -1,17 +1,9 @@
-{ pkgs, lib, username, ... }:
+inputs.nixos-wsl.nixosModules.default
 {
-  imports = [
-    # include NixOS-WSL modules
-    <nixos-wsl/modules>
-  ];
-
   wsl = {
     enable = true;
     defaltUser = username;
     wslConf.network.generatedReslovConfm = false;
     networking.nameservers = [ "1.1.1.1" "9.9.9.9" ];
   };
-
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-}
+};
