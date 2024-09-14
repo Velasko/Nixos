96f7f424fb645a9288e71ed816c124832748a16a
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2024-09-13 23:30:10 -0300

    Disabling wayland

--- system/bootable/modules/kde.nix
+++ system/bootable/modules/kde.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = false;
+  };
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
