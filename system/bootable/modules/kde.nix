e31fc136b553b5ff9c6b65eee76e4739557f501d
Luís Filipe Velasco da Silva <f.l.velasko@gmail.com>
2024-09-14 00:53:48 -0300

    Setting auto numlock

--- system/bootable/modules/kde.nix
+++ system/bootable/modules/kde.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }:
 {
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    autoNumlock = true;
+  };
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
