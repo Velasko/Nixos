c466819584c344e7e05a73efcf7d1e0142377cc2
Velasco <f.l.velasko@gmail.com>
2025-01-31 23:45:53 -0300

    Reenabling kde

--- system/bootable/modules/kde.nix
+++ system/bootable/modules/kde.nix
@@ -7,7 +7,7 @@
   };
 
   programs.hyprland.enable = true;
-  services.desktopManager.plasma6.enable = false;
+  services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
     konsole
