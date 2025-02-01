6a0e7cfdc4018e7de6b1e2919579ba6c6dc39b35
Velasco <f.l.velasko@gmail.com>
2025-02-01 12:25:23 -0300

    multiple hyprland changes

--- home/velasco/modules/hyperland.nix
+++ home/velasco/modules/hyperland.nix
@@ -1,34 +1,41 @@
 { inputs, pkgs, lib, username, ... }: {
 	imports = [ ./waybar.nix ];
 
-	home.packages = with pkgs; [ libpng ];
+	home.packages = with pkgs; [ libpng eww ];
 
 	wayland.windowManager.hyprland = {
 		enable = true;
 		xwayland.enable = true;
+		systemd.enable = true;
 		settings = {
 			"$terminal" = "${pkgs.alacritty}/bin/alacritty";
 
 			exec-once = [
-				"${pkgs.waybar}/bin/waybar"
-				"${pkgs.hyprpaper}/bin/hyprpaper"
-				"${pkgs.networkmanagerapplet}/bin/nm-applet --indicator"
+				"${pkgs.waybar}/bin/waybar" # utility bar
+				"${pkgs.hyprpaper}/bin/hyprpaper" # wallpaper
+				"${pkgs.networkmanagerapplet}/bin/nm-applet --indicator" # wifi manager tray
+				"${pkgs.swaynotificationcenter}/bin/swaync"
+				"systemctl --user start ${pkgs.hyprpolkitagent}/bin/hyprpolkitagent"
 			];
 
-			input.kb_layout = "br";
+			input = {
+				kb_layout = "br";
+				touchpad.natural_scroll = true;
+			};
 
 			"$super" = "SUPER";
 
 			bind = [
 				"$super, T, exec, $terminal"
-				"$super, M, exit,"
+				"$super, N, exec, ${pkgs.swaynotificationcenter}/bin/swaync-client -t"
+				"$super, R, exec, ${pkgs.rofi}/bin/rofi -show run"
+				"$super, L, exit,"
 			];
 
-		misc = {
-			force_default_wallpaper = 0;
-			disable_hyprland_logo = true;
-		};
-
+			misc = {
+				force_default_wallpaper = 0;
+				disable_hyprland_logo = true;
+			};
 		};
 	};
 }
