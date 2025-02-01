52892d4fbf37323acf3a552c307360ad11369f5f
Velasco <f.l.velasko@gmail.com>
2025-01-31 23:45:40 -0300

    Reindenting stylix

--- home/velasco/modules/stylix.nix
+++ home/velasco/modules/stylix.nix
@@ -1,14 +1,15 @@
 { username, pkgs, config, ... }: {
-  stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/${config.programs.zsh.sessionVariables.BASE16_THEME}.yaml";
-  stylix.cursor.package = pkgs.apple-cursor;
-  stylix.cursor.name = "macOS-BigSur-White";
+  	stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/${config.programs.zsh.sessionVariables.BASE16_THEME}.yaml";
+	stylix.cursor.package = pkgs.apple-cursor;
+	stylix.cursor.name = "macOS-BigSur-White";
 
-  stylix.image = ../../../home/${username}/wallpaper.png;
-  stylix.polarity = "dark";
-  stylix.enable = true;
+	stylix.image = ../../../home/${username}/wallpaper.png;
+	stylix.polarity = "dark";
+	stylix.enable = true;
 
-  stylix.targets.btop.enable = true;
-  stylix.targets.lazygit.enable = true;
-  stylix.targets.alacritty.enable = true;
+	stylix.targets = {
+		hyprland.enable = true;
+		hyprpaper.enable = true;
+	};
 }
 
