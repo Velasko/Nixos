b856cce7333ec81f5529deeb7be40612e44a5c1d
Velasco <f.l.velasko@gmail.com>
2025-02-01 01:33:15 -0300

    Changed wallpaper to jpeg, instead of png

--- home/velasco/modules/stylix.nix
+++ home/velasco/modules/stylix.nix
@@ -1,15 +1,17 @@
-{ username, pkgs, config, ... }: {
+{ pkgs, config, username, ... }: {
   	stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/${config.programs.zsh.sessionVariables.BASE16_THEME}.yaml";
 	stylix.cursor.package = pkgs.apple-cursor;
 	stylix.cursor.name = "macOS-BigSur-White";
 
-	stylix.image = ../../../home/${username}/wallpaper.png;
+	stylix.image = ../../../home/${username}/wallpaper.jpeg;
 	stylix.polarity = "dark";
 	stylix.enable = true;
 
 	stylix.targets = {
 		hyprland.enable = true;
 		hyprpaper.enable = true;
+		gtk.enable = true;
+		gnome.enable = true;
 	};
 }
 
