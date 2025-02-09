cf179900af904d46784bed971ac745e8bc3f8f95
Velasco <f.l.velasko@gmail.com>
2025-02-09 16:28:36 -0300

    Reindenting

--- home/velasco/environments/main.nix
+++ home/velasco/environments/main.nix
@@ -1,21 +1,21 @@
 { pkgs, ... }: {
-  imports = [ ../modules/hyperland.nix ];
+	imports = [ ../modules/hyperland.nix ];
 
-  home.packages = with pkgs; [
-    bitwarden-desktop
-    bitwarden-cli
-    discord
-	openvpn
-    spotify
-    telegram-desktop
-    vesktop
-    unzip
-    vlc
-    wine
-  ];
+	home.packages = with pkgs; [
+		bitwarden-desktop
+		bitwarden-cli
+		discord
+		openvpn
+		spotify
+		telegram-desktop
+		vesktop
+		unzip
+		vlc
+		wine
+	];
 
-  home.sessionVariables = {
-    STEAM_EXTRA_COMPAT_TOOLS_PATH = "\${HOME}/.steam/root/compatibility.d";
-    LD_LIBRARY_PATH = "${pkgs.nix-ld}/lib:$LD_LIBRARY_PATH";
-  };
+	home.sessionVariables = {
+	STEAM_EXTRA_COMPAT_TOOLS_PATH = "\${HOME}/.steam/root/compatibility.d";
+	LD_LIBRARY_PATH = "${pkgs.nix-ld}/lib:$LD_LIBRARY_PATH";
+	};
 }
