69d2993a11e305075cb27b5dc00df15641456e14
Velasco <f.l.velasko@gmail.com>
2025-02-10 20:36:26 -0300

    Adding mouse binds

--- home/velasco/modules/hyperland.nix
+++ home/velasco/modules/hyperland.nix
@@ -29,8 +29,13 @@
       monitor = [ ", preferred, auto, 1" ];
 
       "$super" = "SUPER";
+      "$ssuper" = "SUPER SHIFT";
 
       bind = import ./hyprlandbinds.nix { inherit pkgs; };
+      bindm = [
+        "$super, mouse:273, resizewindow"
+        "$super, mouse:272, movewindow"
+      ];
 
       misc = {
         force_default_wallpaper = 0;
