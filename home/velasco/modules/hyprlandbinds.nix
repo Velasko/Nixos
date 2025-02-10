e2a09007d7820111d22a15cc7b35ac47c664a6f1
Velasco <f.l.velasko@gmail.com>
2025-02-10 20:37:00 -0300

    Adding some nice binds

--- home/velasco/modules/hyprlandbinds.nix
+++ home/velasco/modules/hyprlandbinds.nix
@@ -1,13 +1,15 @@
 { pkgs, ... }: [
   # Base functionaliny
-  "$super, F, togglefloating"
+  "$super, page_up, fullscreen" # super + F (xremap)
+  "$ssuper, page_up, togglefloating" # super + shift + F (xremap)
   "$super, L, exit,"
   "$super, N, exec, ${pkgs.swaynotificationcenter}/bin/swaync-client -t"
-  "$super, Q, exec, $terminal"
+  "$super, Q, killactive"
   "$super, R, exec, ${pkgs.rofi}/bin/rofi -show run"
-  "$super, T, exec, $terminal"
+  "$super, T, exec, ${pkgs.alacritty}/bin/alacritty"
 
   # Application shortcuts
-  "$super, 1, exec, ${pkgs.yazi}/bin/yazi"
+  "$super, 1, exec, ${pkgs.alacritty}/bin/alacritty --class yazi -e ${pkgs.yazi}/bin/yazi"
+  "$super, 2, exec, ${pkgs.firefox}/bin/firefox"
 
 ]
