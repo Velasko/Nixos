6e8b3107be05ec5f9b98d790dc88cab609bc60ad
Velasco <f.l.velasko@gmail.com>
2025-02-11 02:55:43 -0300

    Changing hypridle to use the correct locking

--- home/velasco/modules/hyprland/hypridle.nix
+++ home/velasco/modules/hyprland/hypridle.nix
@@ -7,15 +7,15 @@
     enable = true;
     settings = {
       general = {
-        lock_cmd = lib.getExe config.programs.hyprlock.package;
-        before_sleep_cmd = "${pkgs.systemd}/bin/loginctl lock-session";
+        lock_cmd = "${pkgs.hyprlock}/bin/hyprlock";
+        before_sleep_cmd = "${pkgs.hyprlock}/bin/hyprlock";
         after_sleep_cmd = "${pkgs.hyprland}/bin/hyprctl dispatch dpms on";
       };
 
       listener = [
         {
           timeout = 300;
-          on-timeout = "${pkgs.systemd}/bin/loginctl lock-session";
+          on-timeout = config.services.hypridle.settings.general.lock_cmd;
         }
         {
           timeout = 330;
