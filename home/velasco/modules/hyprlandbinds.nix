b1c9daea38bd674c1e45c8677a1964e7d00d3102
Velasco <f.l.velasko@gmail.com>
2025-02-10 21:22:58 -0300

    Adding focus and window management

--- home/velasco/modules/hyprlandbinds.nix
+++ home/velasco/modules/hyprlandbinds.nix
@@ -8,6 +8,16 @@
   "$super, R, exec, ${pkgs.rofi}/bin/rofi -show run"
   "$super, T, exec, ${pkgs.alacritty}/bin/alacritty"
 
+  # Arrow movement
+  "alt, Up, movefocus, u"
+  "alt, Left, movefocus, l"
+  "alt, Right, movefocus, r"
+  "alt, Down, movefocus, d"
+  "super alt, up, movewindow, u"
+  "super alt, Left, movewindow, l"
+  "super alt, Right, movewindow, r"
+  "super alt, Down, movewindow, d"
+
   # Application shortcuts
   "$super, 1, exec, ${pkgs.alacritty}/bin/alacritty --class yazi -e ${pkgs.yazi}/bin/yazi"
   "$super, 2, exec, ${pkgs.firefox}/bin/firefox"
