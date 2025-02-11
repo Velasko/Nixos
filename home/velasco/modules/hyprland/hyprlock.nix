ea41f0f36a9ad66680eba6ede7f330658eb8ca31
Velasco <f.l.velasko@gmail.com>
2025-02-11 02:57:46 -0300

    Rverted to the stylized lock screen

--- home/velasco/modules/hyprland/hyprlock.nix
+++ home/velasco/modules/hyprland/hyprlock.nix
@@ -1,4 +1,4 @@
-{ config, ... }: {
+{ config, lib, ... }: {
   programs.hyprlock = {
     enable = true;
     settings =
@@ -11,36 +11,36 @@
           hide_cursor = true;
         };
 
-        # background = [
-        #   {
-        #     monitor = "";
-        #     path = "screenshot";
-        #     blur_passes = 2;
-        #     blur_size = 2;
-        #     new_optimizations = true;
-        #     ignore_opacity = false;
-        #   }
-        # ];
+        background = lib.mkForce [
+          {
+            monitor = "";
+            path = "screenshot";
+            blur_passes = 2;
+            blur_size = 2;
+            new_optimizations = true;
+            ignore_opacity = false;
+          }
+        ];
 
-        # input-field = [
-        #   {
-        #     monitor = "";
-        #     size = "300, 50";
-        #     outline_thickness = 2;
-        #     outer_color = "rgb(${color.base03})";
-        #     inner_color = "rgb(${color.base00})";
-        #     font_color = "rgb(${color.base07})";
-        #     # check_color = "rgb(${color.base0B})";
-        #     fail_color = "rgb(${color.base08})";
-        #     fade_on_empty = false;
-        #     placeholder_text = "<i>Enter Password</i>";
-        #     dots_spacing = 0.2;
-        #     dots_center = true;
-        #     position = "0, 125";
-        #     valign = "bottom";
-        #     halign = "center";
-        #   }
-        # ];
+        input-field = lib.mkForce [
+          {
+            monitor = "";
+            size = "300, 50";
+            outline_thickness = 2;
+            outer_color = "rgb(${color.base03})";
+            inner_color = "rgb(${color.base00})";
+            font_color = "rgb(${color.base07})";
+            # check_color = "rgb(${color.base0B})";
+            fail_color = "rgb(${color.base08})";
+            fade_on_empty = false;
+            placeholder_text = "<i>Enter Password</i>";
+            dots_spacing = 0.2;
+            dots_center = true;
+            position = "0, 125";
+            valign = "bottom";
+            halign = "center";
+          }
+        ];
 
         label = [
           {
