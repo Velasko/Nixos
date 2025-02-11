3938da23b1869d70e154d639aa8a4f90ad886cfd
Velasco <f.l.velasko@gmail.com>
2025-02-11 01:36:25 -0300

    Lockscreens not working; commenting for later

--- home/velasco/modules/hyprland/hyprlock.nix
+++ home/velasco/modules/hyprland/hyprlock.nix
@@ -11,36 +11,36 @@
           hide_cursor = true;
         };
 
-        background = [
-          {
-            monitor = "";
-            path = "screenshot";
-            blur_passes = 2;
-            blur_size = 2;
-            new_optimizations = true;
-            ignore_opacity = false;
-          }
-        ];
+        # background = [
+        #   {
+        #     monitor = "";
+        #     path = "screenshot";
+        #     blur_passes = 2;
+        #     blur_size = 2;
+        #     new_optimizations = true;
+        #     ignore_opacity = false;
+        #   }
+        # ];
 
-        input-field = [
-          {
-            monitor = "";
-            size = "300, 50";
-            outline_thickness = 2;
-            outer_color = "rgb(#${color.base03})";
-            inner_color = "rgb(#${color.base00})";
-            font_color = "rgb(#${color.base07})";
-            check_color = "rgb(#${color.base0B})";
-            fail_color = "rgb(#${color.base08})";
-            fade_on_empty = false;
-            placeholder_text = "<i>Enter Password</i>";
-            dots_spacing = 0.2;
-            dots_center = true;
-            position = "0, 125";
-            valign = "bottom";
-            halign = "center";
-          }
-        ];
+        # input-field = [
+        #   {
+        #     monitor = "";
+        #     size = "300, 50";
+        #     outline_thickness = 2;
+        #     outer_color = "rgb(${color.base03})";
+        #     inner_color = "rgb(${color.base00})";
+        #     font_color = "rgb(${color.base07})";
+        #     # check_color = "rgb(${color.base0B})";
+        #     fail_color = "rgb(${color.base08})";
+        #     fade_on_empty = false;
+        #     placeholder_text = "<i>Enter Password</i>";
+        #     dots_spacing = 0.2;
+        #     dots_center = true;
+        #     position = "0, 125";
+        #     valign = "bottom";
+        #     halign = "center";
+        #   }
+        # ];
 
         label = [
           {
@@ -48,7 +48,7 @@
             text = "cmd[update:3600000] date +'%A, %B %d'";
             font_family = "Inconsolata Nerd Font Propo Bold";
             font_size = 36;
-            color = "rgb(#${color.base07})";
+            color = "rgb(${color.base07})";
             position = "0, -150";
             valign = "top";
             halign = "center";
@@ -58,7 +58,7 @@
             text = "$TIME";
             font_family = "Inconsolata Nerd Font Propo Bold";
             font_size = 132;
-            color = "rgb(#${color.base07})";
+            color = "rgb(${color.base07})";
             position = "0, -200";
             valign = "top";
             halign = "center";
@@ -68,7 +68,7 @@
             text = "   $USER";
             font_family = "Inconsolata Nerd Font Propo Bold";
             font_size = 24;
-            color = "rgb(#${color.base07})";
+            color = "rgb(${color.base07})";
             position = "0, 200";
             valign = "bottom";
             halign = "center";
