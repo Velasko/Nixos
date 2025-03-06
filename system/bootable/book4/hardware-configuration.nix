a95991dce3ec300fd00f2e03eee0252a33d667bc
Velasco <f.l.velasko@gmail.com>
2025-03-06 14:41:51 -0300

    Reindenting book4 config

--- system/bootable/book4/hardware-configuration.nix
+++ system/bootable/book4/hardware-configuration.nix
@@ -4,31 +4,32 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-	imports =
-	[ (modulesPath + "/installer/scan/not-detected.nix")
-	];
+  imports =
+    [
+      (modulesPath + "/installer/scan/not-detected.nix")
+    ];
 
-	boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "rtsx_usb_sdmmc" ];
-	boot.initrd.kernelModules = [ ];
-	boot.kernelModules = [ "kvm-intel" ];
-	boot.extraModulePackages = [ ];
-	boot.kernelParams = [
-		"mem_sleep_default=s2idle"
-		"acpi_osi=!"
-		"\"acpi_osi=Windows 2022\"" 
-	];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "rtsx_usb_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+  boot.kernelParams = [
+    "mem_sleep_default=s2idle"
+    "acpi_osi=!"
+    "\"acpi_osi=Windows 2022\""
+  ];
 
 
-	# Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-	# (the default) this is the recommended approach. When using systemd-networkd it's
-	# still possible to use this option, but it's recommended to use it in conjunction
-	# with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-	networking.useDHCP = lib.mkDefault true;
-	# networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
-	# networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
-	nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
-	hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-	powerManagement.cpuFreqGovernor = "powersave";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  powerManagement.cpuFreqGovernor = "powersave";
 }
