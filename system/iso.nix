bc4f71282772d65b4f386ce245fd7f25d303e001
Velasco <f.l.velasko@gmail.com>
2025-03-30 01:02:50 -0300

    Disko: Showing ip addresses on startup

--- system/iso.nix
+++ system/iso.nix
@@ -63,6 +63,9 @@
     serviceConfig.ExecStart = [ "" "@${pkgs.util-linux}/sbin/agetty agetty --login-program ${config.services.getty.loginProgram} --autologin ${username} --noclear --keep-baud %I 115200,38400,9600 $TERM" ];
   };
 
+  environment.etc."issue.d/ip.issue".text = "\\4\n";
+  networking.dhcpcd.runHook = "${pkgs.utillinux}/bin/agetty --reload";
+
   environment.systemPackages = with pkgs; [
     git
     gum
