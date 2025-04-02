daec609cdff7e9a19d45887cfe269c747d3e15e7
Velasco <f.l.velasko@gmail.com>
2025-04-02 00:36:00 -0300

    Emacs: tab management

--- home/velasco/modules/emacs.nix
+++ home/velasco/modules/emacs.nix
@@ -9,12 +9,50 @@
 	programs.emacs = {
 		enable = true;
 		package = pkgs.emacs-nox;
-		extraPackages = pkgs: [ pkgs.dracula-theme ];
-		extraConfig = ''
-			(custom-set-variables
-				'(xterm-mouse-mode t))
-				 '(cua-mode t)
-				 '(desktop-save-mode t)
-		'';
+		extraPackages = pkgs: with pkgs; [
+		  all-the-icons
+			centaur-tabs
+			vertico
+		];
 	};
+
+	home.file.".emacs".text = ''
+(custom-set-variables
+	'(xterm-mouse-mode t)
+	'(cua-mode t)
+	'(desktop-save-mode t)
+	'(save-place-mode t)
+	'(global-display-line-numbers-mode t)
+)
+
+(use-package centaur-tabs
+  :init
+  (centaur-tabs-mode t)
+  (setq centaur-tabs-enable-key-bindings t)
+  (setq centaur-tabs-style "wave"
+        centaur-tabs-cycle-scope 'tabs
+        centaur-tabs-ace-jump t
+
+        ;; tab selected highlight:
+        centaur-tabs-set-bar 'under
+        x-underline-at-descent-line t
+
+        ;; Close/modified icon:
+        ;;centaur-tabs-close-button "X"
+        centaur-tabs-set-modified-marker t
+        ;; centaur-tabs-modified-marker "*"
+
+        ;; not working:
+        centaur-tabs-set-icons t
+  )
+  :config
+  (centaur-tabs-headline-match)
+  :bind
+  ("C-<prior>" . centaur-tabs-backward)
+  ("C-<next>" . centaur-tabs-forward)
+  ("C-S-<prior>" . centaur-tabs-move-current-tab-to-left)
+  ("C-S-<next>" . centaur-tabs-move-current-tab-to-right)
+  ("C-M-<next>" . centaur-tabs-ace-jump)
+)
+'';
 }
